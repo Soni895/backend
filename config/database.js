d8ea798b706dbef7fdd4ae75140627873c4c6917
soni895 <darshansoni895@gmail.com>
2023-11-19 17:11:30 +0530

    my sql data base added

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 // Importing the Mongoose library
 const mongoose = require("mongoose");
+const mysql= require("mysql");
 
 // Importing the environment variables using the dotenv library
 require("dotenv");
@@ -20,7 +21,26 @@ const dbConnect = () => {
 			console.error(err.message);
 			process.exit(1);
 		});
+
+      // Create a connection to the database
+  mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'yourpassword',
+	database: "ExpressIntegration"       
+  })
+  // If the connection is successful, log a success message
+  .then(() => console.log("DB CONNECTION SUCCESS"))
+  // If there are issues connecting to the database, log an error message and exit the process
+  .catch((err) => {
+	  console.log(`DB CONNECTION ISSUES`);
+	  console.error(err.message);
+	  process.exit(1);
+  });
+
 };
 
 // Exporting the dbConnect function for use in other files
 module.exports = dbConnect;
+
+
