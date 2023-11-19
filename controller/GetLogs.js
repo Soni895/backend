099f44be3022052743ae6f27006f47cecc35127a
soni895 <darshansoni895@gmail.com>
2023-11-19 20:47:16 +0530

    code update

--- controller/GetLogs.js
+++ controller/GetLogs.js
@@ -31,11 +31,52 @@ exports.GetAllLogs= async(req,res)=>
 exports.GetLogs= async(req,res)=>
 {
     try {
-        const {level,message,resourceId,timestamp,traceId,spanId,commit,metadata}= req.body;
-        console.log(level,message,resourceId,timestamp,traceId,spanId,commit,metadata)
-        const  response= await  Logs.find({level});
-        console.log(response);
+        const {level,message,resourceId,timestamp,traceId,spanId,commit,id}= req.body;
+        console.log(level,message,resourceId,timestamp,traceId,spanId,commit,id);
+        let response;
+        if(level)
+        {
+              response= await  Logs.find({level});
 
+        }
+        if(message)
+        {
+              response= await  Logs.find({message});
+        }
+        if(resourceId)
+        {
+            response= await  Logs.find({resourceId});
+
+        }
+        if(timestamp)
+        {
+            response= await  Logs.find({timestamp});
+
+        }
+       if(commit)
+       {
+        response= await  Logs.find({commit});
+
+       }
+       if(spanId)
+       {
+        response= await  Logs.find({spanId});
+
+       }
+       if(traceId)
+       {
+        response= await  Logs.find({traceId});
+
+
+       }
+       if(id)
+       {
+        response= await  Logs.find({_id:id});
+
+       }
+       
+
+        console.log(response);
        return  res.status(200).json(
             {
                 status:true,
